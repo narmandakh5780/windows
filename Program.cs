115b185b6d5ef5f11e1d353078420a8fe15b3c20
narmandakh5780 <63303134+narmandakh5780@users.noreply.github.com>
2021-02-17 14:34:43 +0700

    naiztaigaa solison huwilbar

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using Narmandakh.Lab1;
-
+using Undarmaa.Lab1;
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // undarmaagiin project
+            var myObj = new Undarmaa.Lab1.Task2 { City = "Ulaanbaatar" };
+            Console.WriteLine(myObj.City);
+
             Task1 a = new Task1();
             a.Age = Console.ReadLine();
             Console.WriteLine("nas: "+a.Age);
+
+            
         }
     }
 }
---
+++ classUndarmaa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Undarmaa
+{
+    namespace Lab1
+    {
+        class Task2
+        {
+            private string _city;
+            public string City
+            {
+                get { return _city; }
+                set { _city = value; }
+            }
+
+        }
+    }
+}
+
