7e293ed5131fa389b8089f65196af04e825e9d42
narmandakh5780 <63303134+narmandakh5780@users.noreply.github.com>
2021-02-17 13:42:52 +0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Narmandakh
+{
+    namespace Lab1
+    {
+        class Task1
+        {
+            private string _age;
+            public string Age
+            {
+                get
+                {
+                    return _age;
+                }
+                set
+                {
+                    _age = value;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Narmandakh.Lab1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1 a = new Task1();
+            a.Age = Console.ReadLine();
+            Console.WriteLine("nas: "+a.Age);
+        }
+    }
+}
